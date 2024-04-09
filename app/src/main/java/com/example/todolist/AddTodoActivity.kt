5fc32e746c080d1b3fa3e88b5471f6d2e0b378ad
Pearl <rkdwlswn227@gmail.com>
2024-04-09 13:06:25 +0900

    edit: activities

--- app/src/main/java/com/example/todolist/AddTodoActivity.kt
+++ app/src/main/java/com/example/todolist/AddTodoActivity.kt
@@ -2,10 +2,14 @@ package com.example.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.todolist.databinding.ActivityAddTodoBinding
 
 class AddTodoActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityAddTodoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityAddTodoBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_add_todo)
     }
 }
--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.example.todolist
 
+import android.os.Build.VERSION_CODES.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.todolist.databinding.ActivityMainBinding
+import com.example.todolist.db.AppDatabase
+import com.example.todolist.db.TodoDao
+import com.example.todolist.db.TodoEntity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    private lateinit var db : AppDatabase
+    private lateinit var todoDao : TodoDao
+    private lateinit var todoList : ArrayList<TodoEntity>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+
+        db = AppDatabase.getInstance(this)!!
+        todoDao = db.getTodoDao()
+
+        getAllTodoList()
+
     }
+
+    private fun getAllTodoList(){
+        Thread{
+            todoList = ArrayList(todoDao.getAllTodo())
+            setRecyclerView() //recyclerview를 설정해서 보여주기기
+        }
+    }
+
+    private fun setRecyclerView(){
+
+    }
+
 }
